c69e2888b3fe7fd36bfe837b2e69423110e359e8
MukhtarYusuf <mukhtar.yusuf1989@gmail.com>
2016-07-21 04:21:47 -0400

    Fixed gradle build bugs, Added new Activity
    - Fixed gradle old plugin error
    - Minor changes to Main Activity
    - Added new Activity

--- StockFinder/app/src/main/java/com/example/mukhtaryusuf/stockfinder/MainActivity.java
+++ StockFinder/app/src/main/java/com/example/mukhtaryusuf/stockfinder/MainActivity.java
@@ -1,14 +1,11 @@
 package com.example.mukhtaryusuf.stockfinder;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -33,28 +30,31 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
-        results = (TextView) findViewById(R.id.textViewResult);
-        processButton = (Button) findViewById(R.id.buttonProcess);
+//        results = (TextView) findViewById(R.id.textViewResult);
+//        processButton = (Button) findViewById(R.id.buttonProcess);
         companyListView = (ListView) findViewById(R.id.listViewResult);
 
-        symbols = new ArrayList<String>(Arrays.asList("GOOG","AAPL","YHOO","MSFT","ADBE"));
+        symbols = new ArrayList<String>(Arrays.asList("IBM","ORCL","GOOG","AAPL","YHOO","MSFT","ADBE","EBAY"));
 
-        processButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Synchronizer synchronizer = new Synchronizer(symbols);
-                synchronizer.execute();
-            }
-        });
+        Synchronizer synchronizer = new Synchronizer(symbols);
+        synchronizer.execute();
+
+//        processButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Synchronizer synchronizer = new Synchronizer(symbols);
+//                synchronizer.execute();
+//            }
+//        });
     }
 
     @Override
