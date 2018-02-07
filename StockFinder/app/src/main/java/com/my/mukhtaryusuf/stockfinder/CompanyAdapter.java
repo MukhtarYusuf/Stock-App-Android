43ed25adff6f3f8aec33ac76bf56c961094feb5e
MukhtarYusuf <mukhtar.yusuf1989@gmail.com>
2018-02-07 17:03:05 -0500

    Minor updates

---
+++ StockFinder/app/src/main/java/com/my/mukhtaryusuf/stockfinder/CompanyAdapter.java
@@ -0,0 +1,101 @@
+package com.my.mukhtaryusuf.stockfinder;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Filter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by mukhtaryusuf on 7/13/16.
+ */
+
+public class CompanyAdapter extends ArrayAdapter<Company> {
+
+    Context context;
+    int resource;
+    ArrayList<Company> originalData;
+    ArrayList<Company> data = new ArrayList<>();
+    ArrayList<Company> filteredData = new ArrayList<>();
+    Filter filter;
+
+    public CompanyAdapter(Context context, int resource, int textViewResourceId, ArrayList<Company> data) {
+        super(context, resource, textViewResourceId, data);
+        this.context = context;
+        this.resource = resource;
+        this.data = data;
+        this.originalData = new ArrayList<>(data);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        super.getView(position, convertView, parent);
+
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView = layoutInflater.inflate(this.resource, parent, false);
+
+        TextView cSymbolTextView = (TextView) rowView.findViewById(R.id.company_symbol);
+        TextView cNameTextView = (TextView) rowView.findViewById(R.id.company_name);
+        TextView sValueTextView = (TextView) rowView.findViewById(R.id.stock_value);
+        TextView pChangeTextView = (TextView) rowView.findViewById(R.id.percentage_change);
+
+        cSymbolTextView.setText(data.get(position).getSymbol());
+        cNameTextView.setText(data.get(position).getName());
+        sValueTextView.setText(data.get(position).getPrice());
+        pChangeTextView.setText(data.get(position).getNetChange());
+
+        if(data.get(position).getNetChange().charAt(0) == '-')
+            pChangeTextView.setTextColor(context.getResources().getColor(R.color.md_red_400));
+        else
+            pChangeTextView.setTextColor(context.getResources().getColor(R.color.md_green_A700));
+        return rowView;
+    }
+
+    @Override
+    public Filter getFilter(){
+        if(filter == null)
+            filter = new CompanyFilter();
+        return filter;
+    }
+
+    public class CompanyFilter extends Filter{
+
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            filteredData.clear();
+            FilterResults filterResults = new FilterResults();
+            String query = constraint.toString().toLowerCase();
+
+            if(query == null || query.length() == 0){
+                filteredData.addAll(originalData);
+                filterResults.values = filteredData;
+                filterResults.count = filteredData.size();
+            }else {
+                for(Company c : originalData){
+                    if(c.getName().toLowerCase().contains(query) || c.getSymbol().toLowerCase().contains(query))
+                        filteredData.add(c);
+                }
+                filterResults.values = filteredData;
+                filterResults.count = filteredData.size();
+            }
+
+            return filterResults;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            ArrayList<Company> fResults = (ArrayList<Company>) results.values;
+            clear();
+
+            for(Company c : fResults) {
+                data.add(c);
+            }
+
+            notifyDataSetChanged();
+        }
+    }
+}
