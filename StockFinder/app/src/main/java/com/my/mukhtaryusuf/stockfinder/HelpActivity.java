5dbc91a7e41ac03a5c305797c932b21614b2cd2c
MukhtarYusuf <mukhtar.yusuf1989@gmail.com>
2018-03-02 01:30:10 -0500

    Added More Content to Help Activity

--- StockFinder/app/src/main/java/com/my/mukhtaryusuf/stockfinder/HelpActivity.java
+++ StockFinder/app/src/main/java/com/my/mukhtaryusuf/stockfinder/HelpActivity.java
@@ -17,19 +17,27 @@ public class HelpActivity extends AppCompatActivity {
 
     //Initialize Labels
     String lprice = "Price";
-    String lNetChange = "Net Change";
+    String lOpen = "Open";
+    String lClose = "Close";
+    String lHigh = "High";
+    String lLow = "Low";
     String lWeekHigh = "Week High";
     String lWeekLow = "Week Low";
-    String lDividend = "Dividend";
     String lVolume = "Volume";
+    String lNetChange = "Net Change";
+    String lMarketCap = "Market Capacity";
 
     //Initialize Definitions
     String dPrice = "The price of the company's stock";
-    String dNetChange = "The change in stock price in the current day compared to the previous day";
+    String dOpen = "The official open price";
+    String dClose = "The official close price";
+    String dHigh = "The market wide highest price";
+    String dLow = "The market wide lowest price";
     String dWeekHigh = "The highest price that the stock has reached in a 52 week period";
     String dWeekLow = "The lowest price that the stock has reached in a 52 week period";
-    String dDividend = "The annual dividend quoted. The value indicates that payments have been made to stockholders";
     String dVolume = "The number of shares of this particular stock that were traded today";
+    String dNetChange = "The change in stock price in the current day compared to the previous day";
+    String dMarketCap = "The market value of the company's outstanding shares";
 
 
     @Override
@@ -40,8 +48,10 @@ public class HelpActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         listView = (ListView) findViewById(R.id.help_list);
-        labels = new ArrayList<>(Arrays.asList(lprice, lNetChange, lWeekHigh, lWeekLow, lDividend, lVolume));
-        definitions = new ArrayList<>(Arrays.asList(dPrice, dNetChange, dWeekHigh, dWeekLow, dDividend, dVolume));
+        labels = new ArrayList<>(Arrays.asList(lprice, lOpen, lClose, lHigh, lLow, lWeekHigh, lWeekLow,
+                lVolume, lNetChange, lMarketCap));
+        definitions = new ArrayList<>(Arrays.asList(dPrice, dOpen, dClose, dHigh, dLow, dWeekHigh, dWeekLow,
+                dVolume, dNetChange, dMarketCap));
 
         HelpAdapter helpAdapter = new HelpAdapter(getApplicationContext(), R.layout.help_row, R.id.help_label, labels, definitions);
         listView.setAdapter(helpAdapter);
