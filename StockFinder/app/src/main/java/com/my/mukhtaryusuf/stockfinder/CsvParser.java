c434a52ae85b77a0ef94bf27bbba884493539e57
MukhtarYusuf <mukhtar.yusuf1989@gmail.com>
2018-03-01 20:02:48 -0500

    Added JsonParser Class

--- StockFinder/app/src/main/java/com/my/mukhtaryusuf/stockfinder/CsvParser.java
+++ StockFinder/app/src/main/java/com/my/mukhtaryusuf/stockfinder/CsvParser.java
@@ -1,10 +1,5 @@
 package com.my.mukhtaryusuf.stockfinder;
 
-import android.util.Log;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-
 /**
  * Created by mukhtaryusuf on 7/14/16.
  */
@@ -14,77 +9,77 @@ public class CsvParser {
 
     //--Note: Consider optimizing parse function--
     //Parse function is dependent on the nature and order of API call as well as Company object
-    public static ArrayList<Company> parse(String data){
-        Log.i(LOG_TAG, data);
-        String[] companyRows = null;
-
-        ArrayList<Company> parsedCompanyList = new ArrayList<Company>();
-
-        //Split string into rows
-        if(data != null && data != "") {
-            //data = data.replace("\"","");
-            companyRows = data.split("\n");
-
-            if(companyRows != null) {
-                for (String s : companyRows) {
-                    ArrayList<String> companyColumnsAsList = new ArrayList<>(); //Makes it possible to construct company columns from lists
-                    String[] testComopanyColumns = null;
-                    String[] testCompanyColumns2 = null;
-                    Company company = null;
-
-                    testComopanyColumns = s.split("\"");
-                    if(testComopanyColumns.length > 4) { //Full Data Retrieved with no N/A results
-
-                        //Split other comma separated data
-                        testCompanyColumns2 = testComopanyColumns[4].split(",");
-
-                        companyColumnsAsList.add(testComopanyColumns[1]); //Add symbol. Skip index 0 because it's a space from split() function
-                        companyColumnsAsList.add(testComopanyColumns[3]); //Add name
-                        companyColumnsAsList.addAll(Arrays.asList(testCompanyColumns2)); //Add rest of data
-                        companyColumnsAsList.add(testComopanyColumns[5]); //Add % change
-
-                        String[] companyFields = new String[companyColumnsAsList.size()];
-                        companyFields = companyColumnsAsList.toArray(companyFields);
-
-                        //Skip companyFields[2] because an empty string from split() is stored there
-                        company = new Company(companyFields[0],
-                                companyFields[1],
-                                companyFields[3],
-                                companyFields[4],
-                                companyFields[5],
-                                companyFields[6],
-                                companyFields[7],
-                                companyFields[8]
-                        );
-                    }else{
-                        testCompanyColumns2 = testComopanyColumns[2].split(",");
-
-                        companyColumnsAsList.add(testComopanyColumns[1]);
-                        companyColumnsAsList.addAll(Arrays.asList(testCompanyColumns2));
-
-                        if(testComopanyColumns.length == 4) //Row has extra quotes at the end for % change
-                            companyColumnsAsList.add(testComopanyColumns[3]);
-
-                        String[] companyFields = new String[companyColumnsAsList.size()];
-                        companyFields = companyColumnsAsList.toArray(companyFields);
-
-                        //Skip companyFields[1] because an empty string from split() is stored there
-                        company = new Company(companyFields[0],
-                                companyFields[2],
-                                companyFields[3],
-                                companyFields[4],
-                                companyFields[5],
-                                companyFields[6],
-                                companyFields[7],
-                                companyFields[8]
-                        );
-                    }
-                    parsedCompanyList.add(company);
-                }
-            }
-            return parsedCompanyList;
-        }else{
-            return null;
-        }
-    }
+//    public static ArrayList<Company> parse(String data){
+//        Log.i(LOG_TAG, data);
+//        String[] companyRows = null;
+//
+//        ArrayList<Company> parsedCompanyList = new ArrayList<Company>();
+//
+//        //Split string into rows
+//        if(data != null && data != "") {
+//            //data = data.replace("\"","");
+//            companyRows = data.split("\n");
+//
+//            if(companyRows != null) {
+//                for (String s : companyRows) {
+//                    ArrayList<String> companyColumnsAsList = new ArrayList<>(); //Makes it possible to construct company columns from lists
+//                    String[] testComopanyColumns = null;
+//                    String[] testCompanyColumns2 = null;
+//                    Company company = null;
+//
+//                    testComopanyColumns = s.split("\"");
+//                    if(testComopanyColumns.length > 4) { //Full Data Retrieved with no N/A results
+//
+//                        //Split other comma separated data
+//                        testCompanyColumns2 = testComopanyColumns[4].split(",");
+//
+//                        companyColumnsAsList.add(testComopanyColumns[1]); //Add symbol. Skip index 0 because it's a space from split() function
+//                        companyColumnsAsList.add(testComopanyColumns[3]); //Add name
+//                        companyColumnsAsList.addAll(Arrays.asList(testCompanyColumns2)); //Add rest of data
+//                        companyColumnsAsList.add(testComopanyColumns[5]); //Add % change
+//
+//                        String[] companyFields = new String[companyColumnsAsList.size()];
+//                        companyFields = companyColumnsAsList.toArray(companyFields);
+//
+//                        //Skip companyFields[2] because an empty string from split() is stored there
+//                        company = new Company(companyFields[0],
+//                                companyFields[1],
+//                                companyFields[3],
+//                                companyFields[4],
+//                                companyFields[5],
+//                                companyFields[6],
+//                                companyFields[7],
+//                                companyFields[8]
+//                        );
+//                    }else{
+//                        testCompanyColumns2 = testComopanyColumns[2].split(",");
+//
+//                        companyColumnsAsList.add(testComopanyColumns[1]);
+//                        companyColumnsAsList.addAll(Arrays.asList(testCompanyColumns2));
+//
+//                        if(testComopanyColumns.length == 4) //Row has extra quotes at the end for % change
+//                            companyColumnsAsList.add(testComopanyColumns[3]);
+//
+//                        String[] companyFields = new String[companyColumnsAsList.size()];
+//                        companyFields = companyColumnsAsList.toArray(companyFields);
+//
+//                        //Skip companyFields[1] because an empty string from split() is stored there
+//                        company = new Company(companyFields[0],
+//                                companyFields[2],
+//                                companyFields[3],
+//                                companyFields[4],
+//                                companyFields[5],
+//                                companyFields[6],
+//                                companyFields[7],
+//                                companyFields[8]
+//                        );
+//                    }
+//                    parsedCompanyList.add(company);
+//                }
+//            }
+//            return parsedCompanyList;
+//        }else{
+//            return null;
+//        }
+//    }
 }
--- StockFinder/app/src/main/java/com/my/mukhtaryusuf/stockfinder/JsonParser.java
+++ StockFinder/app/src/main/java/com/my/mukhtaryusuf/stockfinder/JsonParser.java
@@ -1,5 +1,6 @@
 package com.my.mukhtaryusuf.stockfinder;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -14,14 +15,25 @@ public class JsonParser {
     //--JSON KEYS--
     final static String QUOTE_KEY = "quote";
     final static String NEWS_KEY = "news";
+
     final static String SYMBOL_KEY = "symbol";
     final static String NAME_KEY = "companyName";
     final static String PRICE_KEY = "latestPrice";
+    final static String OPEN_KEY = "open";
+    final static String CLOSE_KEY = "close";
+    final static String HIGH_KEY = "high";
+    final static String LOW_KEY = "low";
     final static String WEEK_HIGH_KEY = "week52High";
     final static String WEEK_LOW_KEY = "week52Low";
     final static String VOLUME_KEY = "latestVolume";
     final static String PE_KEY = "peRatio";
     final static String NET_CHANGE_KEY = "change";
+    final static String MKT_CAP_KEY = "marketCap";
+
+    final static String HEADLINE_KEY = "headline";
+    final static String SOURCE_KEY = "source";
+    final static String DATE_KEY = "datetime";
+    final static String URL_KEY = "url";
 
     public static ArrayList<Company> parse(String jsonString, ArrayList<String> symbols){
         ArrayList<Company> companyList = new ArrayList<>();
@@ -35,13 +47,33 @@ public class JsonParser {
                 String cSymbol = quotesObject.getString(SYMBOL_KEY);
                 String cName = quotesObject.getString(NAME_KEY);
                 String cPrice = quotesObject.getString(PRICE_KEY);
+                String cOpen = quotesObject.getString(OPEN_KEY);
+                String cClose = quotesObject.getString(CLOSE_KEY);
+                String cHigh = quotesObject.getString(HIGH_KEY);
+                String cLow = quotesObject.getString(LOW_KEY);
                 String cWeekHigh = quotesObject.getString(WEEK_HIGH_KEY);
                 String cWeekLow = quotesObject.getString(WEEK_LOW_KEY);
                 String cVolume = quotesObject.getString(VOLUME_KEY);
                 String cPE = quotesObject.getString(PE_KEY);
                 String cNetChange = quotesObject.getString(NET_CHANGE_KEY);
+                String cMarketCap = quotesObject.getString(MKT_CAP_KEY);
 
-                Company company = new Company(cSymbol,cName,cPrice,cWeekHigh,cWeekLow,cVolume,cPE,cNetChange);
+                JSONArray newsJSONArray = companyObject.getJSONArray(NEWS_KEY);
+                ArrayList<CompanyNews> cNews = new ArrayList<>();
+                for(int j = 0; j < newsJSONArray.length(); j++){
+                    JSONObject newsJSONObject = newsJSONArray.getJSONObject(j);
+                    String headLineString = newsJSONObject.getString(HEADLINE_KEY);
+                    String sourceString = newsJSONObject.getString(SOURCE_KEY);
+                    String dateString = newsJSONObject.getString(DATE_KEY);
+                    String urlString = newsJSONObject.getString(URL_KEY);
+
+                    CompanyNews companyNews = new CompanyNews(headLineString, sourceString, dateString, urlString);
+                    cNews.add(companyNews);
+                }
+
+                Company company = new Company(cSymbol,cName,cPrice, cOpen, cClose, cHigh,
+                        cLow, cWeekHigh,cWeekLow,cVolume,cPE,cNetChange, cMarketCap);
+                company.setCompanyNews(cNews);
                 companyList.add(company);
             }
         }catch(JSONException e){
